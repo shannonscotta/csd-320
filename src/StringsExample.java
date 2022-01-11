505ddab9ffa3240f20f9f3ffd43a62f8300eb40b
Scott Shannon <scottshannon@Scotts-iMac.local>
2022-01-10 22:17:36 -0800

    example of strings

---
+++ src/StringsExample.java
@@ -0,0 +1,10 @@
+public class StringsExample {
+    public static void main(String[] args) {
+        
+        String s1 = "Normal string";
+        String s2 = "This " + "is " + "concatanated.";
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
