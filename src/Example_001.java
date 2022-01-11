89142e48e2097e9a260d80fd1ed3ab75966d9556
Scott Shannon <scottshannon@Scotts-iMac.local>
2022-01-10 20:25:34 -0800

    example_001 added

---
+++ src/Example_001.java
@@ -0,0 +1,11 @@
+public class Example_001 {
+    public static void main(String [] args){
+
+        double height = 20.0;
+        double width = 30.0;
+
+        double area = height * width;
+
+        System.out.println("The total area is " + area);
+    }
+}
